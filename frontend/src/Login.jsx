c9aa1704c240c131051fdb6add772a3a9c98ade4
unknown <samuel.kimcs97@gmail.com>
2025-10-02 16:47:16 -0700

    complete styling and layout for login page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -8,14 +8,37 @@ export default function Login(props) {
     return (
 		<div className='login-page-case'>
 			<img id='main-logo' src='/src/assets/MainLogo.png'></img>
-            <form className='login-box'>
-                <label for='username'> Username</label>
-                <div id='username-case'>
-                    <img src='/src/assets/user.png'></img>
-                    <input type='text' id='username'></input>
+            <form className='form'>
+
+                <div className='login-box'>
+                    <label>Username</label>
+                    <div id='username-case'>
+                        <img src='/src/assets/user.png'></img>
+                        <input type='text' className='username' placeholder="Enter Username"></input>
+                        <div className='line'></div>
+                    </div>
+                    <label>Password</label>
+                    <div id='password-case'>
+                        <div>
+                            <img className='lock' src='/src/assets/Lock.png'></img>
+                            <input type='text' className='username' placeholder="Enter Password"></input>
+                        </div>
+                        <img className='show-password-button' src='/src/assets/ShowPass.png'></img>
+                        <div className='line'></div>
+                    </div>
+                    <div style={{display: "flex", justifyContent: "flex-end", padding: "10px"}}>
+                        <a href='' className='forgot-password'>Forgot Password?</a>
+                    </div>
                 </div>
-                <span>Enter Username Here</span>
-                <div> Password</div>
+
+                <div style={{display: "flex", justifyContent: "center", paddingTop: "40px"}}>
+                    <button className='login-button'>Log In</button>
+                </div>
+
+                <div style={{display: "flex", justifyContent: "center", paddingTop: "100px"}}>
+                    <span>Don't have an account? </span>
+                    <a style={{marginLeft: "20px"}} href=''>Sign Up</a>
+                </div>
             </form>
 		</div>
 )}
