a94f97a71411b5af4ab347691923ac5e32dffc0b
unknown <samuel.kimcs97@gmail.com>
2025-10-07 21:02:43 -0700

    added sign out page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div id='main-case'>
-      <SignUp/>
+      <SignOut/>
     </div>
   )
 }
--- frontend/src/sign-out/sign-out.jsx
+++ frontend/src/sign-out/sign-out.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
 import React from 'react'
-import './Login.css'
+import './sign-out.css'
 
-export default function Login(props) {
+export default function SignOut(props) {
     return (
         <div id='sign-out-page-case'>
-            
+            <div className='title'>Sign Out?</div>
+            <div className='main-container'>
+                <div style={{marginBottom: "60px", marginTop: "20px"}} className='choice'>Yes</div>
+                <div style={{marginBottom: "20px"}} className='choice'>No</div>
+            </div>
         </div>
 )}
