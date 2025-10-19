0159b57c06efa13571758e99beea12338111cc55
ShaneKerr2001 <skerr2001@gmail.com>
2025-10-19 16:12:51 -0700

    Updated Users Table for token

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -1,7 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timedelta
 
 Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String, 
+
 
 class User(Base):
     __tablename__ = "users"
@@ -9,3 +13,6 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     role = Column(String, default="user")
+    is_verified = Column(Boolean, default=False)
+    verification_token = Column(String, nullable=True)
+    token_expiration = Column(DateTime, nullable=True)
