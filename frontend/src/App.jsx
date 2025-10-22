c8a68fbdcb95f4579aa331414e5530b3118aba4e
Sam Kim <41707531+samuel129@users.noreply.github.com>
2025-10-22 12:03:40 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Login from './login/login.jsx'
+import Login from './login/Login.jsx'
 import SignUp from './sign-up/Sign-Up.jsx'
 import SignOut from './sign-out/sign-out.jsx'
 import CreateReminder  from './create-reminder/Create-Reminder.jsx'
